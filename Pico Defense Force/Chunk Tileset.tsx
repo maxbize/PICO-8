da4965f14fe41df0f64d7867b7003ab51388bb6e
Max Bize <mbize2005@gmail.com>
2021-10-09 21:14:43 -0700

    Proof of concept map graphics with tiles -> chunks -> map

---
+++ Pico Defense Force/Chunk Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Chunk Tileset" tilewidth="64" tileheight="64" tilecount="128" columns="16">
+ <image source="Chunks Map.png" width="1024" height="512"/>
+</tileset>
