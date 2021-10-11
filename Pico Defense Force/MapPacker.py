50a727a4bc17a7234176360802f5aec12e7eef1c
Max Bize <mbize2005@gmail.com>
2021-10-10 17:03:15 -0700

    Implement horizontal and vertical flipping for map chunks

--- Pico Defense Force/MapPacker.py
+++ Pico Defense Force/MapPacker.py
@@ -14,10 +14,14 @@ for str_val in data:
 	
 	# Read and clear the flip flags. See https://doc.mapeditor.org/en/stable/reference/tmx-map-format/#tmx-tilelayer-tile
 	flags = 0
-	flags += (val & 0x80000000) >> 29
-	flags += (val & 0x40000000) >> 29
-	flags += (val & 0x20000000) >> 29
+	flags += (val & 0x80000000) >> 28 # Horizontal
+	flags += (val & 0x40000000) >> 28 # Vertical
+	flags += (val & 0x20000000) >> 28 # Diagonal
 	val &= ~0xe0000000
+
+	# Diagonal flip not supported yet
+	if flags & 0x2:
+		raise Exception(f"Diagonal flip at index:{len(flag_hex)} x:{len(flag_hex) % 64} y:{int(len(flag_hex) / 64)} flags:{flags:#x}")
 
 	# Write the tile ID and flags
 	map_hex += f'{val:0{2}x}' # 0{2} == pad to two digits
