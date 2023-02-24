7d70810cd59fed1435a194494384b1b7792c4eca
Max Bize <mbize2005@gmail.com>
2023-02-23 22:36:30 -0800

    Pixel perfect collision. Load maps into tables

--- Driftmania/driftmania.lua
+++ Driftmania/driftmania.lua
@@ -6,19 +6,27 @@
 --------------------
 local objects = {}
 local player = nil
-local projectile_m = nil
+
+-- Current map sprites / chunks. map[x][y] -> sprite/chunk index
+local map_road_tiles = nil
+local map_road_chunks = nil
+local map_prop_tiles = nil
+local map_prop_chunks = nil
 
 --------------------
 -- Data
 --------------------
-local map_data = '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001020303040501020303060700000000000000000008090a0b090c08090a0d090e0700000000000000000f10111213140f10110015090e07000000000000000f1400000f140f14000000150916000000000000000f1400000f140f14000000000f14000000000000000f1400000f140f14000000000f14000000000000000f1400001709091800000019091a000000000000000f140000121b1c11000019091d1e000000000000001f092000000000000019091d1e0000000000000000212209200000000019091d1e0000000000000000000021220903030303091d1e00000000000000000000000021230d0d0d0d241e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-local prop_data = '000000000000000000000000000000000000000000000025262727282925262727272a0000000000000000252b000000002c2b000000002d2a000000000000002e0000000000000000000000002d2a0000000000002f000030310000000030282900002d2a00000000002f00002f2f000000002f002c2900002f00000000002f00002f2f000000002f00003200002f00000000002f00002f2f000000002f00003300002f00000000002f00002f34000000003300353600002f00000000002f00003437380000353635360000393a00000000002f00003738373b3c3635360000393a0000000000002d2a0000373b27273c360000393a00000000000000002d2a0000000000000000393a000000000000000000002d2a000000000000393a0000000000000000000000002d2727272727273a000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+local map_road_data = '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001020303040501020303060700000000000000000008090a0b090c08090a0d090e0700000000000000000f10111213140f10110015090e07000000000000000f1400000f140f14000000150916000000000000000f1400000f140f14000000000f14000000000000000f1400000f140f14000000000f14000000000000000f1400001709091800000019091a000000000000000f140000121b1c11000019091d1e000000000000001f092000000000000019091d1e0000000000000000212209200000000019091d1e0000000000000000000021220903030303091d1e00000000000000000000000021230d0d0d0d241e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+local map_prop_data = '000000000000000000000000000000000000000000000025262727282925262727272a0000000000000000252b000000002c2b000000002d2a000000000000002e0000000000000000000000002d2a0000000000002f000030310000000030282900002d2a00000000002f00002f2f000000002f002c2900002f00000000002f00002f2f000000002f00003200002f00000000002f00002f2f000000002f00003300002f00000000002f00002f34000000003300353600002f00000000002f00003437380000353635360000393a00000000002f00003738373b3c3635360000393a0000000000002d2a0000373b27273c360000393a00000000000000002d2a0000000000000000393a000000000000000000002d2a000000000000393a0000000000000000000000002d2727272727273a000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
 
 --------------------
 -- Built-in Methods
 --------------------
 
 function _init()
+  map_road_chunks, map_road_tiles = load_map(map_road_data, 21, 3)
+  map_prop_chunks, map_prop_tiles = load_map(map_prop_data, 21, 3)
+
   spawn_player()
 end
 
@@ -32,13 +40,14 @@ end
 function _draw()
   cls(0)
 
-  draw_map(map_data)
+  draw_map(map_road_chunks, 21, 3)
 
   for obj in all(objects) do
     obj.draw(obj)
   end
 
-  draw_map(prop_data)
+  draw_map(map_prop_chunks, 21, 3)
+  _player_debug_draw(player)
 
 end
 
@@ -207,71 +216,110 @@ function _player_draw(self)
   end
   palt(0, true)
   palt(15, false)
+end
 
-  --function pd_rotate(x,y,rot,mx,my,w,flip,scale)
-  --pd_rotate(self.x,self.y,-self.angle_fwd,72,3,2,false,1)
+function _player_debug_draw(self)
+  -- Collision point visualization
+  pset(self.x, self.y, 3)
 
-  -- debugging
-  --local look_x, look_y = angle_vector(self.angle_fwd, self.speed + 5)
-  --line(self.x, self.y, self.x + look_x * 5, self.y + look_y * 5, 1)
-  --local speed_x, speed_y = angle_vector(self.angle_vel, self.speed + 5)
-  --line(self.x, self.y, self.x + speed_x * 5, self.y + speed_y * 5, 3)
+  -- Front/back collision points
+  for i = -1, 1, 2 do
+    for j = -1, 1, 2 do
+      local x = flr(self.x) + cos(self.angle_fwd + 0.1 * i) * 4 * j
+      local y = flr(self.y) + sin(self.angle_fwd + 0.1 * i) * 4 * j
+      pset(x, y, collides_at(x, y) and 8 or 11)
+    end
+  end
+
+--  -- Side collision points
+--  for i = -1, 1, 2 do
+--    local x = flr(self.x) + cos(self.angle_fwd + 0.25 * i) * 2
+--    local y = flr(self.y) + sin(self.angle_fwd + 0.25 * i) * 2
+--    pset(x, y, collides_at(x, y) and 8 or 11)
+--  end
+
+end
+
+-- Checks if the given position on the map overlaps a wall
+local collision_sprites = {[43]=true, [44]=true, [45]=true, [59]=true, [60]=true, [61]=true}
+function collides_at(x, y)
+  local sprite_index = map_prop_tiles[flr(x/8)][flr(y/8)]
+  if sprite_index == nil then
+    return false
+  end
+  if collision_sprites[sprite_index] then
+    local sx = (sprite_index % 16) * 8 + x % 8
+    local sy = flr(sprite_index / 16) * 8 + y % 8
+    local col = sget(sx, sy)
+    return col == 6
+  end
+  return false
 end
 
 --------------------
 -- Map
 --------------------
 
-function draw_map(data)
-  -- Constants for the map string (todo: move to string header)
-  local chunk_size = 3
-  local pix_per_chunk = chunk_size * 8
-  local map_size = 21 -- 21x21 chunk map (63x63 tiles)
+-- TODO: move map_size, chunk_size to data header?
+function load_map(data, map_size, chunk_size)
   local chunks_per_row = flr(128/chunk_size)
+  
+  -- Initialize tables
+  local map_tiles = {}
+  local map_chunks = {}
 
+  -- Parse data
+  local num_chunks = #data / 2 -- todo: when we move to compression we should store this in the header
+  for i = 0, num_chunks - 1 do
+    -- The actual chunk index
+    local chunk_index = tonum("0x" .. sub(data, i * 2 + 1, i * 2 + 2))
+
+    -- "chunk map" x, y
+    local chunk_x = i % map_size
+    local chunk_y = flr(i / map_size)
+    if map_chunks[chunk_x] == nil then map_chunks[chunk_x] = {} end
+    map_chunks[chunk_x][chunk_y] = chunk_index
+
+    -- top left corner of chunk in pico8 tile map
+    local top_left_tile_x = (chunk_index % chunks_per_row) * chunk_size
+    local top_left_tile_y = flr(chunk_index / chunks_per_row) * chunk_size
+    for x = 0, 2 do
+      for y = 0, 2 do
+        local sprite_index = mget(top_left_tile_x + x, top_left_tile_y + y)
+        local tile_x = chunk_x * chunk_size + x
+        local tile_y = chunk_y * chunk_size + y
+        if map_tiles[tile_x] == nil then map_tiles[tile_x] = {} end
+        map_tiles[tile_x][tile_y] = sprite_index
+      end
+    end
+  end
+
+  return map_chunks, map_tiles
+end
+
+function draw_map(map_chunks, map_size, chunk_size)
   -- Find the map index of the top-left map segment
   local camera_x = peek2(0x5f28)
   local camera_y = peek2(0x5f2a)
   local top_left = flr((camera_x + 64) / 32) + 64 * flr((camera_y + 64) / 32)
 
-  -- Draw all map segments surrounding the player
-  for x = -2, 2 do
-    for y = -2, 2 do
-      --local data_index = top_left + x + 64 * y
-      --local map_tile = tonum("0x" .. sub(data, data_index * 2 + 1, data_index * 2 + 2))
-      --map_tile -= 1 -- Tiled uses index 0 for "empty" but we use it as tile 0
-      --map(map_tile % 32 * 4, flr(map_tile / 32) * 4, data_index % 64 * 32, flr(data_index / 64) * 32, 4, 4)
+
+  local chunks_per_row = flr(128/chunk_size)
+
+  for x = 1, map_size - 1 do
+    for y = 1, map_size - 1 do
+      local chunk_index = map_chunks[x][y]
+
+      -- top left corner of chunk in pico8 tile map
+      local tile_x = (chunk_index % chunks_per_row) * chunk_size
+      local tile_y = flr(chunk_index / chunks_per_row) * chunk_size
+
+      -- top left corner of chunk in world
+      local world_x = x * chunk_size * 8
+      local world_y = y * chunk_size * 8
+
+      map(tile_x, tile_y, world_x, world_y, chunk_size, chunk_size)
+
     end
   end
-
---  for x = 0, map_size - 1 do
---    for y = 0, map_size - 1 do
---      local data_index = y * map_size + x
---      local map_tile = tonum("0x" .. sub(data, data_index * 2 + 1, data_index * 2 + 2))
---      map(map_tile % pix_per_chunk * chunk_size, flr(map_tile / pix_per_chunk) * chunk_size, x * pix_per_chunk, y * pix_per_chunk, chunk_size, chunk_size)
---    end
---  end
-
--- MAP(TILE_X, TILE_Y, [SX, SY], [TILE_W, TILE_H], [LAYERS])
-
-  for i = 0, #data / 2 - 1 do
-    -- The actual chunk index
-    local chunk_index = tonum("0x" .. sub(data, i * 2 + 1, i * 2 + 2))
-
-    -- top left corner of chunk in pico8 tile map
-    local tile_x = (chunk_index % chunks_per_row) * chunk_size
-    local tile_y = flr(chunk_index / chunks_per_row) * chunk_size
-
-    -- Re-interpret i into a "chunk map" x, y
-    local x_index = i % map_size
-    local y_index = flr(i / map_size)
-
-    -- top left corner of chunk in world
-    local world_x = x_index * chunk_size * 8
-    local world_y = y_index * chunk_size * 8
-
-    map(tile_x, tile_y, world_x, world_y, chunk_size, chunk_size)
-
-  end
-
 end
